531774ab3125ce8fe217f3ee8ac3f79882940ec8
José Manuel Luman <jluman@ecomsur.com>
2022-12-25 01:15:34 -0300

    final update

--- home/js/functions.js
+++ home/js/functions.js
@@ -38,13 +38,16 @@ const bounceLinkedin = () => {
 
 const toBrasil = () => {
   const r = setInterval(function(){
+    const conteiner = document.querySelector('.container-pdf');
     alert("Hola...");
     alert("Felicidades por encontrar el código qr dentro de la mochila jajaja...")
     alert("Tienes que esperar un poco ya que tu regalo se está generando en este momento ;)...");
     alert("Esta será una de las muchas aventuras que viviremos..."); 
     alert("Espero te guste mucho.. :9");
     alert("PD: Te amo <3")
+    conteiner.getElementsByClassName.diplay = "block"
     clearInterval(r)
-  }, 5000)
+
+  }, 8000)
 }
 
