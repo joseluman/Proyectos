5d434866438de6a900266e2e3a7b4e6df10067c3
José Manuel Luman <jluman@ecomsur.com>
2022-12-25 00:41:14 -0300

    update audio

--- home/js/functions.js
+++ home/js/functions.js
@@ -1,15 +1,16 @@
 window.onload = function() {
     //sliderPrincipal();
     //bounceLinkedin();
-    alert("Hola...");
+    /*alert("Hola...");
     alert("Felicidades por encontrar el código qr dentro de la mochila jajaja...")
     alert("Tienes que esperar un poco ya que tu regalo se está generando en este momento ;)...");
     alert("Esta será una de las muchas aventuras que viviremos..."); 
     alert("Espero te guste mucho.. :9");
-    alert("PD: Te amo <3")
+    alert("PD: Te amo <3")*/
+    toBrasil();
 };
 
-const sliderPrincipal = () => {
+/*const sliderPrincipal = () => {
     var swiper = new Swiper(".mySwiper", {
         loop: true,
         navigation: {
@@ -33,5 +34,17 @@ const bounceLinkedin = () => {
     });
 
 
+}*/
+
+const toBrasil = () => {
+  const r = setInterval(function(){
+    alert("Hola...");
+    alert("Felicidades por encontrar el código qr dentro de la mochila jajaja...")
+    alert("Tienes que esperar un poco ya que tu regalo se está generando en este momento ;)...");
+    alert("Esta será una de las muchas aventuras que viviremos..."); 
+    alert("Espero te guste mucho.. :9");
+    alert("PD: Te amo <3")
+    clearInterval(r)
+  }, 1000)
 }
 
